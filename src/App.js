ce608478ec9805c587d63b14a25f54aeedae8694
jh080724 <tsj010813@gmail.com>
2024-09-10 16:21:08 +0900

    연도별 필터링 구현

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import Expenses from './components/Expense/Expenses';
 //import NoName from './NoName';
 import NewExpense from './components/NewExpense/NewExpense';
+import ExpenseFilter from './components/Expense/ExpenseFilter';
 
 //리엑트 진입점
 function App() {
   const expenses = [
     { id: 1, title: '냠냠치킨', price: 19000, date: new Date(2023, 6, 19) },
     { id: 2, title: '양파', price: 5000, date: new Date(2023, 6, 20) },
+    { id: 3, title: '푸라닥치킨', price: 5000, date: new Date(2023, 6, 20) },
+    { id: 4, title: '버거킹', price: 5000, date: new Date(2021, 6, 20) },
   ];
 
   // 지출 객체 배열을 상태변수로 관리
@@ -24,14 +27,12 @@ function App() {
 
     setExpenseList([...expenseList, modifyEx]);
     console.log(expenseList);
-    console.log('-----------------');
-
-    console.log(modifyEx);
   };
 
   return (
     // React.Fragment 생략가능 -> <> 닫는 tag 반드시 있어야 함. </>
     <>
+      <ExpenseFilter />
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenseList} />
     </>
