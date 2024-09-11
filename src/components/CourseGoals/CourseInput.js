abd8d8b983f70c855db5e4c68881f40f73b9ee6c
jh080724 <tsj010813@gmail.com>
2024-09-11 11:17:00 +0900

    styled-component

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CourseInput.css';
-import Button from '../UI/Button';
+// import Button from '../UI/Button';
+import ButtonComponent from '../UI/Button';
 
-const CourseInput = () => {
+const CourseInput = ({ onAdd }) => {
+  const [enteredText, setEnteredText] = useState('');
+  // 입력값 검증용 상태 변수
+  const [isValid, setIsValid] = useState(true);
+
+  const textChangeHandler = (e) => {
+    if (e.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredText(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault(); // form 기능 중지
+    if (enteredText.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
+    onAdd(enteredText); // App.js에게 입력값 넘기기
+    setEnteredText(''); // 입력창 비우기
+  };
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>나의 목표</label>
-        <input type='text' />
+        <input type='text' value={enteredText} onChange={textChangeHandler} />
       </div>
-      <Button type='submit'>목표 추가하기</Button>
+      <ButtonComponent type='submit'>목표 추가하기</ButtonComponent>
     </form>
   );
 };
 
 export default CourseInput;
+
+// import React from 'react';
+// import './CourseInput.css';
+// import Button from '../UI/Button';
+
+// const CourseInput = () => {
+//   return (
+//     <form>
+//       <div className='form-control'>
+//         <label>나의 목표</label>
+//         <input type='text' />
+//       </div>
+//       <Button type='submit'>목표 추가하기</Button>
+//     </form>
+//   );
+// };
+
+// export default CourseInput;
--- src/components/CourseGoals/CourseList.js
+++ src/components/CourseGoals/CourseList.js
@@ -1,12 +1,28 @@
+// import React from 'react';
+// import './CourseList.css';
+// import CourseItem from './CourseItem';
+
+// const CourseList = ({ items }) => {
+//   return (
+//     <ul className='goal-list'>
+//       {items.map((item) => {
+//         return <CourseItem key={item.id} item={item} />;
+//       })}
+//     </ul>
+//   );
+// };
+
+// export default CourseList;
+
 import React from 'react';
 import './CourseList.css';
 import CourseItem from './CourseItem';
 
-const CourseList = ({ items }) => {
+const CourseList = ({ items, onDelete }) => {
   return (
     <ul className='goal-list'>
       {items.map((item) => {
-        return <CourseItem key={item.id} item={item} />;
+        return <CourseItem key={item.id} item={item} onDelete={onDelete} />;
       })}
     </ul>
   );
