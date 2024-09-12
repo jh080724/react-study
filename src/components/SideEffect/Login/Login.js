3e3327b8ba6c1628baf7752657fc2b4315107f13
jh080724 <tsj010813@gmail.com>
2024-09-12 16:45:53 +0900

    useEffect useEffect 추가 예시 및 clear 함수 사용

--- src/components/SideEffect/Login/Login.js
+++ src/components/SideEffect/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../../UI/Card';
 import styles from './Login.module.css';
 import ButtonComponent from '../../UI/Button';
@@ -11,28 +11,46 @@ const Login = ({ onLogin }) => {
   const [enteredPw, setEnteredPw] = useState('');
 
   // 이메일 입력이 정상적인지 확인
-  const [emailIsValid, setEmailIsValid] = useState();
+  const [emailIsValid, setEmailIsValid] = useState(true);
 
   // 패스워드 입력도 정상적인지 확인
-  const [pwIsValid, setPwIsValid] = useState();
+  const [pwIsValid, setPwIsValid] = useState(true);
 
   // 이메일, 패스워드 둘 다 동시에 정상적인지 확인.
   const [formIsValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+    console.log(' Login.js에서 useEffect 호출됨');
+
+    // setTimeout 을 사용해서 유효성 검증을 1초 뒤에 실행하도록 작성.
+    // 1초 이내에 새로운 입력값이 들어 온다면? -> 상태 변경 -> userEffect가 다시 실행됨.
+    const timer = setTimeout(() => {
+      console.log(' Login.js에서 setTimeout() 호출됨');
+      setFormIsValid(enteredEmail.includes('@') && enteredPw.trim().length > 6);
+    }, 1000);
+
+    // cleanup 함수 - 컴포넌트가 업데이트 되거나, 없어지기 직전에 실행
+    // 사용자가 1초 이내에 추가입력 -> 상태변경(컴포넌트 업데이트) -> 위에 예약한 timer를 취소해라.
+    return () => {
+      clearTimeout(timer);
+    };
+    // 위존성 배열레 상태변수를 넣어주면 그 상태변수가 변경될 때 마다 useEffect가 다시 호출된다.
+  }, [enteredEmail, enteredPw]);
 
   // 이메일이 변경될 때마다 실행할 핸들러
   const emailChangeHandler = (e) => {
     setEnteredEmail(e.target.value);
 
-    setFormIsValid(e.target.value.includes('@') && enteredPw.trim().length > 6);
+    // setFormIsValid(e.target.value.includes('@') && enteredPw.trim().length > 6);
   };
 
   // 비밀번호가 변경될때 마다 실행할 핸들러
   const passwordChangeHandler = (e) => {
     setEnteredPw(e.target.value);
 
-    setFormIsValid(
-      e.target.value.trim().length > 6 && enteredEmail.includes('@'),
-    );
+    // setFormIsValid(
+    //   e.target.value.trim().length > 6 && enteredEmail.includes('@'),
+    // );
   };
 
   const validateEmailHandler = () => {
