30ba2f3f970caf260131d3dc24b68cc78d45402d
jh080724 <tsj010813@gmail.com>
2024-09-13 11:39:51 +0900

    useReduce, ContextAPI

--- src/components/SideEffect/Navigation/Navigation.js
+++ src/components/SideEffect/Navigation/Navigation.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './Navigation.module.css';
+import AuthContext from '../store/auth-context';
 
-const Navigation = ({ isLoggedIn, onLogout }) => {
+const Navigation = () => {
+  // Context store에 좀 더 쉽게 접근해서 소비할 수 있게 해주는 useContext hook.
+  // AuthContext에서 제공하는 전역 객체를 디스트럭쳐링으로 분해.
+  const { isLoggedIn, onLogout } = useContext(AuthContext);
+
   return (
     <nav className={classes.nav}>
       {isLoggedIn && (
         <ul>
           <li>
-            <a href='#'>my Page</a>
+            <a href='#'>MyPage</a>
           </li>
           <li>
             <a href='#'>Admin</a>
